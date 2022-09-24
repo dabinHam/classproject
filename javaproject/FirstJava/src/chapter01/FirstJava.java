6799b335010123185548eae5c34e450db3993822
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-09-24 21:54:44 +0900

    main 메소드

---
+++ javaproject/FirstJava/src/chapter01/FirstJava.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class FirstJava {
+
+	// main 메소드가 클래스의 시작포인트
+	public static void main(String[] args) {
+
+		System.out.println(21);
+		System.out.println("Hello~ JAVA!!!");
+
+	}
+
+}
