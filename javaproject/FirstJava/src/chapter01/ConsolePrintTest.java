d2481c4c677cc9ee4fa0ad5bed7c9a46869ee8fc
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-09-24 21:55:15 +0900

    출력

---
+++ javaproject/FirstJava/src/chapter01/ConsolePrintTest.java
@@ -0,0 +1,18 @@
+package chapter01;
+
+public class ConsolePrintTest {
+
+	public static void main(String[] args) {
+
+		// \n : 한줄 띄워줌
+		// \t : tab 만큼 띄워줌
+		// \" , \' : 인용문
+
+		System.out.print("안녕하세요.\n  함다빈 \t\t 입니다.");
+		System.out.print("\nJAVA는 \"객체지향 언어\" 입니다. \n"); // JAVA는 "객체지향 언어" 입니다.
+		System.out.print("\nJAVA는 \'객체지향 언어\' 입니다. \n"); // JAVA는 '객체지향 언어' 입니다.
+		System.out.println("C:\\Users\\cheoho-hi"); // C:\Users\cheoho-hi
+
+	}
+
+}
