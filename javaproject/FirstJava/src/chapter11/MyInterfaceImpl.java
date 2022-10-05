a8e2b22596526f0c279991cb02f7983be38ad697
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-10-05 10:21:43 +0900

    제네릭 인터페이스
    제네릭 인터페이스를 생성하고 추상메소드로 구현해보고
    제네릭타입을 2가지 해본 예문

--- javaproject/FirstJava/src/chapter11/MyInterfaceImpl.java
+++ javaproject/FirstJava/src/chapter11/MyInterfaceImpl.java
@@ -1,8 +1,8 @@
 package chapter11;
-
+// 제네릭은 클래스뿐만 아니라 메소드 매개변수, 반환타입으로도 사용가능하다.
+// 제네릭 인터페이스를 생성하여 추상메소드로 구현 할 수있다.
 public class MyInterfaceImpl<T1, T2> implements MyInterface<T1, T2> {
 
-	
 	@Override
 	public T1 method1(T1 t) {
 		return t;
@@ -14,14 +14,13 @@ public class MyInterfaceImpl<T1, T2> implements MyInterface<T1, T2> {
 	}
 
 	public static void main(String[] args) {
-		
+
 		// <> 안에 기본형<T1,T2> 가 아닌 참조형<String,Integer>이 와야한다.
-		MyInterfaceImpl<String,Integer> myClass = new MyInterfaceImpl<String, Integer>();
-		
+		MyInterfaceImpl<String, Integer> myClass = new MyInterfaceImpl<String, Integer>();
+
 		System.out.println("Ten");
 		System.out.println((10));
-		
+
 	}
-	
-	
+
 }
