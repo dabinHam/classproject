9341d9807989f98da33b18787af775245ea12792
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-10-07 12:39:25 +0900

    스레드

---
+++ javaproject/FirstJava/src/chapter13/MyThread2.java
@@ -0,0 +1,19 @@
+package chapter13;
+
+public class MyThread2 implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=0; i<10; i++) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} // 현재 스레드의 상태를 block 상태로 변경
+		System.out.println("Runnable 인터페이스를 이용한 스레드 생성과 작업이 진행됩니다.");
+		}
+	}
+	
+	
+}
