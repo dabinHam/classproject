49b9f4cbb112e923d4930475a19c35a8b925f816
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-08 17:32:09 +0900

    수정, edit
    Mapper 의 sql문 오타 #{writer}으로 써야하는데 #{write}로 해서 오류남
    
    - BoardWriteService
    if (file != null && !file.isEmpty() && file.getSize()>0){ !부정표시 안해줘서 사진이 업데이트 되지 않았었음.

--- spring/boot/SpringBootBoard/src/main/java/com/app/board/mapper/BoardMapper.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/mapper/BoardMapper.java
@@ -12,19 +12,22 @@ public interface BoardMapper {
     @Select("select * from tbl_board")
     List<BoardDTO> selectAll();
 
-    @Select("select * from tbl_board order by bno desc limit ${param1},#{param2}")
+    @Select("select * from tbl_board order by bno desc limit #{start},#{count}")
     List<BoardDTO> selectList(@Param("start") int start, @Param("count")int count);
 
     @Select("select count(*) from tbl_board")
-    Integer totalCount();
+    Integer totalCount();   // 전체 게시물의 개수 구하기
 
     // 게시글 하나의 정보 읽기
     @Select("select * from tbl_board where bno=#{no}")
     BoardDTO selectByBno(int bno);
 
-    @Insert("insert into tbl_board (title,content,writer,photo)VALUES(#{title},#{content},#{write},#{photo})")
+    @Insert("insert into tbl_board (title,content,writer,photo)VALUES(#{title},#{content},#{writer},#{photo})")
     Integer insert(BoardDTO boardDTO) throws SQLException;
 
     @Delete("delete from tbl_board where bno=#{bno}") // parameter가 하나밖에 없기때문에 이름을 뭘로 적어도 상관없다.
     Integer deleteByBno(int bno);
+
+    @Update("update tbl_board set title=#{title}, content=#{content}, writer=#{writer}, photo=#{photo}, updatedate=now() where bno=#{bno}")
+    Integer update(BoardDTO boardDTO) throws SQLException;
 }
--- spring/boot/SpringBootBoard/src/main/java/com/app/board/service/BoardDeleteService.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/service/BoardDeleteService.java
@@ -1,9 +1,12 @@
 package com.app.board.service;
 
 
+import com.app.board.domain.BoardDTO;
 import com.app.board.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class BoardDeleteService {
@@ -12,6 +15,25 @@ public class BoardDeleteService {
     private BoardMapper boardMapper;
 
     public int delete(int bno){
-        return boardMapper.deleteByBno(bno);
+
+        // 삭제 하려는 게시물의 데이터
+        BoardDTO boardDTO = boardMapper.selectByBno(bno);
+
+        // 삭제 결과 확인
+        int result = boardMapper.deleteByBno(bno);
+
+        // 해당
+        if(result>0 && boardDTO.getPhoto()!=null){
+
+            File delFile = new File(new File("").getAbsolutePath(),"photo"+File.separator+boardDTO.getPhoto());
+            if (delFile.exists()){
+                delFile.delete();
+            }
+
+
+        }
+
+//        return boardMapper.deleteByBno(bno);
+        return result;
     }
 }
