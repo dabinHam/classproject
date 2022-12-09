72b1ccd0d6a5c2ebe276d9e1e044d2feba85c335
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-09 15:17:41 +0900

    reply modal 추가

--- spring/boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ReplyRestController.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ReplyRestController.java
@@ -2,19 +2,23 @@ package com.app.board.controller.board;
 
 
 import com.app.board.domain.ReplyDTO;
+import com.app.board.service.ReplyDeleteService;
+import com.app.board.service.ReplyInsertSerivce;
 import com.app.board.service.ReplyListService;
+import com.app.board.service.ReplyReadService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.ReactiveTransaction;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@Log4j2
 @RequestMapping("/reply")
 public class ReplyRestController {
     // get  /reply => list
@@ -25,6 +29,14 @@ public class ReplyRestController {
     @Autowired
     private ReplyListService replyListService;
 
+    @Autowired
+    private ReplyInsertSerivce replyInsertSerivce;
+
+    @Autowired
+    private ReplyReadService replyReadService;
+
+    @Autowired
+    private ReplyDeleteService replyDeleteService;
     // get  /reply/{bno} => list
     @GetMapping(value = "/{bno}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ReplyDTO>> selectList(@PathVariable("bno") int bno){
@@ -35,9 +47,28 @@ public class ReplyRestController {
     }
 
 
-    // post /reply => reply
+    // post /reply => reply  |  클라이언트로부터 JSON데이터를 받아서 DB에 insert  처음 받을시 rno가 없어서
+    @PostMapping
+    public ResponseEntity<ReplyDTO> insertReply(@RequestBody ReplyDTO replyDTO){
+
+        log.info("insert 전 "+replyDTO);
+        /// Service -> Mapper
+        replyInsertSerivce.insertReply(replyDTO);
+
+        log.info("insert 후 "+replyDTO);     // rno 값이 갱신된 데이터
+
+        replyDTO.setReplyDate(LocalDate.now().toString());
+
+        return new ResponseEntity<>(replyReadService.selectByRno(replyDTO.getRno()), HttpStatus.OK);
+    }
+
     // put  /reply/{rno} => reply
-    // delete /reply/{rno} => 0 / 1, ok, fail
+    // delete /reply/{rno} => 0 / 1, ok, success,fail
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<Integer> delete(@PathVariable("rno")int rno){     /*처리완료 : 0 , 처리실패 : 1*/
+        return new ResponseEntity<>(replyDeleteService.deleteByRno(rno), HttpStatus.OK);
+
+    }
 
 
 
--- spring/boot/SpringBootBoard/src/main/java/com/app/board/mapper/ReplyMapper.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/mapper/ReplyMapper.java
@@ -1,8 +1,7 @@
 package com.app.board.mapper;
 
 import com.app.board.domain.ReplyDTO;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -11,5 +10,16 @@ public interface ReplyMapper {
 
     @Select("select * from tbl_reply where bno=#{bno}")
     List<ReplyDTO> selectAll(int bno);
+
+    @Select("select * from tbl_reply where rno=#{rno}")
+    ReplyDTO selectByRno(int bno);
+
+
+    @Insert("INSERT INTO tbl_reply(bno,reply,replyer) VALUES(#{bno},#{reply},#{replyer})")
+    @Options(useGeneratedKeys = true, keyProperty = "rno",keyColumn = "rno") // 자동생성된 키값 사용
+    int insertReply(ReplyDTO replyDTO);
+
+    @Delete("delete form tbl_reply where rno=#{rno}")
+    int deleteByRno(int rno);
 
 }
