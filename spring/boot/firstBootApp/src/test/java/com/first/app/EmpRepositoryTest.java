cc9c652a4c81829cdf1cb3874ee07c58f1f8f86a
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-19 00:57:03 +0900

    pagingTest
    page는 0부터 시작!! 1부터 해서 오류발생하지않게 주의하기!
    화면에 보이는 1페이지 = page 0
    
    list에서 필요한 데이터를 미리 계산을하고 처리해 줄 수 있음 .

--- spring/boot/firstBootApp/src/test/java/com/first/app/EmpRepositoryTest.java
+++ spring/boot/firstBootApp/src/test/java/com/first/app/EmpRepositoryTest.java
@@ -6,6 +6,9 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -49,5 +52,26 @@ public class EmpRepositoryTest {
 
     }
 
+    @Test
+    public void empPagingTest(){
+        // page는 0부터 시작!! | 화면에 보이는 1페이지 = page 0
+//        Pageable pageable = PageRequest.of(0,5); //  Pageable : org.springframework.data.domain 로 선택 해주기
+        Pageable pageable = PageRequest.of(3,5); // 4페이지
 
-}
+        Page<Emp> result = empRepository.findAll(pageable); //  pageable로 하게되면 page type으로 반환.
+
+        log.info("############################ ############################ ############################");
+        log.info("result >>>>조회항목 리스트 " + result.getContent());
+        log.info("result >>>>전체 게시물의 개수 " + result.getTotalElements());
+        log.info("result >>>>전체 페이지의 개수 " + result.getTotalPages());
+        log.info("result >>>>현재 요청 페이지 번호 " + result.getNumber());
+        log.info("result >>>>페이지당 표현할 항목의 개수 " + result.getSize());
+        log.info("result >>>>현재 페이지에 출력된 항목의 개수 " + result.getNumberOfElements());
+
+    }
+
+
+
+
+
+}// EmpRepositoryTest end
