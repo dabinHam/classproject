e7d5543ddc1a36295ea180b069fbabfb1eb77642
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-27 16:25:28 +0900

    조건부 렌더링 추가 설명

--- react/test/src/LoginControl.jsx
+++ react/test/src/LoginControl.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 
     // props를 통하여 onClick에 함수를 전달받으려고 함. 
     // <button onClick={props.onClick}>  여기서의 onClick은 속성이름
+    // props.onClick의 부분은 내가 선언하는 변수명같은거임 . 이름이 같으면 헷갈릴수있어서 수정.
     // return의 값은 단순 UI ,  props(속성)을 통해서 이벤트를 받아옴. 
 
     function LoginButton(props){
         return(
             <>
-                <button onClick={props.onClick}>    
+                <button onClick={props.handleClick}>    
                     로그인
                 </button>
             </>
@@ -18,7 +19,7 @@ import { useState } from "react";
     function LogoutButton(props){
         return(
             <>
-                <button onClick={props.onClick}>    
+                <button onClick={props.handleClick}>    
                     로그아웃
                 </button>
             </>
@@ -39,9 +40,9 @@ import { useState } from "react";
         let btn;
 
         if(isLogin){
-            btn = <LogoutButton onClick={handleLogoutClick}/>
+            btn = <LogoutButton handleClick={handleLogoutClick}/>
         } else{
-            btn = <LoginButton onClick={handleLoginClick}/>
+            btn = <LoginButton handleClick={handleLoginClick}/>
         }
 
         return(
