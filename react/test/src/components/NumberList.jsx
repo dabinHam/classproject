c7d971454ff9dba432644fa08b805b5f0a000b0f
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-28 10:57:03 +0900

    배열 key값 생성
    리스트 아이템에는 무조건 키가 있어야 한다는 경고 문구가 나온다
    이를 해결해 주기 위하여 key 값을 생성해준다.

--- react/test/src/components/NumberList.jsx
+++ react/test/src/components/NumberList.jsx
@@ -5,9 +5,10 @@ function NumberList(props){
     const numbers = [1,2,3,77,5]
 
     // 배열의 map(callback 반환하는 값) => 새로운 배열
-    const listItems= numbers.map((number) => <li>{number}</li>)
+    // const listItems= numbers.map((number) => <li>{number}</li>);
     // map : 새로운 배열을 만든다 = 새로운 요소는 callback(= 반환값이 새로운 요소 )
     // <li>{number}</li> : 엘리먼트
+    const listItems= numbers.map((number,index) => <li key={index}>{number}</li>);
 
     /* [{},{},{},{},{}] */
 
