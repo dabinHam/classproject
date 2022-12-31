b9629c80029f39b6738803e55d68186b8172e8e7
dabinHam <113006964+dabinHam@users.noreply.github.com>
2022-12-31 14:22:07 +0900

    edit
    오타수정

--- react/miniblog/src/components/ui/TextInput.js
+++ react/miniblog/src/components/ui/TextInput.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from "styled-components";
 
 const StyledTextarea = styled.textarea`
-    width: calc(100%-32px);
-    ${props=>props.height && `height:${props.height}px`};
+    width: calc(100% - 32px);
+    ${props => props.height && `height:${props.height}px;`};
     padding:16px;
     font-size:16px;
     line-height:20px;
